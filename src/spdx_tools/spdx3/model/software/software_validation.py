f1760fe2ea51318b8377c482a60a3034c3befb5b
Mike Griffin <michael.griffin@infomagnus.com>
2025-05-30 16:26:45 -0700

    Fix broken import / add Software Validation model

---
+++ src/spdx_tools/spdx3/model/software/software_validation.py
@@ -0,0 +1,5 @@
+class SoftwareValidation:
+    def __init__(self, spdx_id, validation_type=None, comment=None):
+        self.spdx_id = spdx_id
+        self.validation_type = validation_type
+        self.comment = comment
