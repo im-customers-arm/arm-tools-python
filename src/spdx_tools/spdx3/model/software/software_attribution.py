7dc78e76a6e77c0b6ede4bb3ef15193196a27ad8
Mike Griffin <michael.griffin@infomagnus.com>
2025-05-30 16:25:58 -0700

    Fix broken import / add Software Attribution model

---
+++ src/spdx_tools/spdx3/model/software/software_attribution.py
@@ -0,0 +1,5 @@
+class SoftwareAttribution:
+    def __init__(self, spdx_id, attribution_text=None, comment=None):
+        self.spdx_id = spdx_id
+        self.attribution_text = attribution_text
+        self.comment = comment
